0e01b596c5365b368fb248b6297fcc2639d35494
Warlib1975 <2af@mail.ru>
2019-05-26 06:47:47 +0300

    Initial version

---
+++ GSM-modem-AT-commands-tester.ino
@@ -0,0 +1,229 @@
+#define TINY_GSM_MODEM_SIM800
+#include <TinyGsmClient.h>
+#include "GSM_SIM800.h"
+
+#define RXD2 16
+#define TXD2 17
+
+#define MTS
+
+/*class MyTinyGSM : public TinyGsmSim800
+{
+
+};*/
+
+GSM_SIM800 modemGSM(Serial2);
+
+#define SIM_PIN ""
+#ifdef BEELINE
+  #define APN_NAME "internet.beeline.ru"
+  #define APN_USER "beeline"
+  #define APN_PSWD "beeline"
+#else
+  #define APN_NAME "internet.mts.ru"
+  #define APN_USER "mts"
+  #define APN_PSWD "mts"
+#endif
+
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 38400
+#define Max_Modem_Reboots 5
+
+int Modem_Reboots_Counter = 0;
+
+
+void Init_GSM_SIM800() 
+{
+  Serial.println("Initialize GSM modem...");
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("Serial GSM Txd is on GPIO" + String(TXD2));
+  Serial.println("Serial GSM Rxd is on GPIO" + String(RXD2));
+
+  //delay(3000);
+
+  TinyGsmAutoBaud(Serial2, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
+
+  String info = modemGSM.getModemInfo();
+  
+  Serial.println(info);
+
+  if (!modemGSM.restart())
+  {
+    RestartGSMModem();
+  }
+  else
+  {
+    Serial.println("Modem restart OK");
+  }
+
+  
+  Serial.println("Set AT+CLTS=1");
+  modemGSM.sendAT("AT+CLTS=1");
+  modemGSM.waitResponse();
+  delay(3000);
+  modemGSM.sendAT("AT&W");
+  modemGSM.waitResponse();
+  delay(3000);
+  modemGSM.sendAT("AT+CFUN=0");
+  modemGSM.waitResponse();
+  delay(3000);
+  modemGSM.sendAT("AT+CFUN=1");
+  modemGSM.waitResponse();
+  delay(3000);
+
+  if (modemGSM.getSimStatus() != SIM_READY)
+  {
+    Serial.println("Check PIN code for the SIM. SIM status: " + SimStatus(modemGSM.getSimStatus()));
+    if (SIM_PIN != "")
+    {
+     Serial.println("Try to unlock SIM PIN.");
+      modemGSM.simUnlock(SIM_PIN);
+      delay(3000);
+      if (modemGSM.getSimStatus() != 3)
+      {
+        RestartGSMModem();
+      }
+    }
+  }
+ 
+  if (!modemGSM.waitForNetwork()) 
+  {
+    Serial.println("Failed to connect to network");
+    RestartGSMModem();
+  }
+  else
+  {
+    RegStatus registration = modemGSM.getRegistrationStatus();
+    Serial.println("Registration: [" + modemGSM.GSMRegistrationStatus(registration) + "]"); 
+    Serial.println("Modem network OK");
+  }
+  
+  Serial.println(modemGSM.gprsConnect(APN_NAME,APN_USER,APN_PSWD) ? "GPRS Connect OK" : "GPRS Connection failed");
+
+  bool stateGPRS = modemGSM.isGprsConnected();
+  if (!stateGPRS)
+  {
+    RestartGSMModem();
+  }
+  
+  String state = stateGPRS ? "connected" : "not connected";
+  Serial.println("GPRS status: " + state);
+  Serial.println("CCID: " + modemGSM.getSimCCID());
+  Serial.println("IMEI: " + modemGSM.getIMEI());
+  Serial.println("Operator: " + modemGSM.getOperator());
+
+  /*IPAddress local = modemGSM.localIP();
+  Serial.println("Local IP: " + local.toString());*/
+
+  int csq = modemGSM.getSignalQuality();
+  if (csq == 0)
+  {
+    Serial.println("Signal quality is 0. Restart modem.");
+    RestartGSMModem();
+  }
+  Serial.println("Signal quality: " + modemGSM.GSMSignalLevel(csq) + " [" + String(csq) + "]");
+
+  int battLevel = modemGSM.getBattPercent();
+  Serial.println("Battery level: " + String(battLevel) + "%");
+
+  float battVoltage = modemGSM.getBattVoltage() / 1000.0F;
+  Serial.println("Battery voltage: " + String(battVoltage));
+
+  String gsmLoc = modemGSM.getGsmLocation();
+  Serial.println("GSM location: " + gsmLoc);
+  //Serial.println("GSM location: " + SwapLocation(gsmLoc));
+
+  modemGSM.sendAT(GF("AT+CMEE=2"));
+  modemGSM.waitResponse();
+}
+
+
+void RestartGSMModem()
+{
+    Serial.println("Restarting GSM...");
+    if (!modemGSM.restart())
+    {
+      Serial.println("\tFailed. :-(\r\n");
+      //ESP.restart();
+    } 
+    if (Modem_Reboots_Counter < Max_Modem_Reboots)
+    {
+      Init_GSM_SIM800();
+    }
+    Modem_Reboots_Counter++;
+}
+  /*void smsInit()
+  {
+        //modemGSM.sendAT(GF("+CPMS=ME,ME,ME"));
+        modemGSM.sendAT(GF("+CPMS=SM,SM,SM"));
+        modemGSM.waitResponse();
+
+        modemGSM.sendAT(GF("+CMGF=1"));
+        modemGSM.waitResponse();
+
+        modemGSM.sendAT(GF("+CNMI=1,0"));
+        modemGSM.waitResponse();
+
+        modemGSM.sendAT(GF("+CSCS=\"GSM\""));
+        modemGSM.waitResponse();
+  }*/
+
+void setup() {
+  Serial.begin(9600);
+  Init_GSM_SIM800();
+
+  GSM_SIM800::SMSmessage msg = modemGSM.readSMS(2);
+
+  Serial.println("------------------------------------");
+  Serial.println("Index: " + String(msg.Index));
+  Serial.println("Is readed: " + String(msg.IsReaded));
+  Serial.println("Number: " + msg.PhoneNumber);
+  Serial.println("Date: " + msg.Date);
+  Serial.println("Message: " + msg.Message);
+  Serial.println("------------------------------------");
+  
+  //readAllSMSs();
+  /*modemGSM.waitResponse(10000L);
+  delay(10000);
+  String data = modemGSM.stream.readStringUntil('\n');
+  Serial.println("Stop waiting for the response.");  
+  Serial.println("Data: " + data);*/
+}
+
+int lastMillis = 0;
+void loop() 
+{
+  int currentMillis = millis();
+  if (currentMillis - lastMillis > 60000*5)
+  {
+    //readSMS1(6);
+    GSM_SIM800::SMSmessages messages = modemGSM.readSMSs();
+    for (const  GSM_SIM800::SMSmessage message : messages)
+    {
+      Serial.println("Index: " + String(message.Index));
+      Serial.println("Is readed: " + String(message.IsReaded));
+      Serial.println("Number: " + message.PhoneNumber);
+      Serial.println("Date: " + message.Date);
+      Serial.println("Message: " + message.Message);
+    }
+
+    GSM_SIM800::Outputs outputs = modemGSM.processIOOHcommand("IOOH <n1 n 5 n7 >");
+    for (byte output : outputs)
+    {
+      Serial.println("Output: " + String(output));
+    }
+        
+    lastMillis = currentMillis;
+  }
+    
+  if (Serial2.available())
+  {
+    Serial.write(Serial2.read());
+  }
+  if (Serial.available())
+  {
+    String str = Serial.readString();
+    Serial.println("Command: " + str);
+    Serial2.print(str);
+  }
+}
---
+++ GSM_SIM800.h
@@ -0,0 +1,375 @@
+#define TINY_GSM_MODEM_SIM800
+#include <TinyGsmClient.h>
+#include <TimeLib.h>
+
+#define numOfParams 5
+
+enum ReadSMSMode {
+  ReceivedUnread,
+  ReceivedRead,
+  StoredUnsent,
+  StoredSent,
+  AllSMS
+};
+
+enum DeleteSMSMode {
+  Read = 1,
+    Unread = 2,
+    Sent = 3,
+    Unsent = 4,
+    Received = 5,
+    All = 6
+};
+
+class GSM_SIM800: public TinyGsmSim800 {
+  private: bool IsSMSInited = false;
+
+  public:
+
+  GSM_SIM800(Stream & stream): TinyGsmSim800(stream) {}
+
+  
+
+struct SMSmessage
+{
+  int Index;
+  bool IsReaded;
+  String PhoneNumber;
+  String Date;
+  String Message;
+  TimeElements Time;
+  public:
+    void SetTime(String date, bool isPrint = false)
+    {
+      Time = parseDateTime(date);
+      Date = date; 
+    }
+    
+    TimeElements parseDateTime(String date)
+    {
+      TimeElements tm;
+      uint8_t Year, Month, Day, Hour, Minute, Second, MilliSecond;
+      const char* str = date.c_str();//"19/05/05,21:27:58+12";
+      sscanf(str, "%d/%d/%d,%d:%d:%d+%d", &Year, &Month, &Day, &Hour, &Minute, &Second, &MilliSecond);
+      tm.Year = Year; //CalendarYrToTm();
+      tm.Month = Month;
+      tm.Day = Day;
+      tm.Hour = Hour;
+      tm.Minute = Minute;
+      tm.Second = Second;
+      return tm;
+    }
+};
+
+String Trim(String str)
+{
+  str.trim();
+  if (str.length() > 0)
+  {
+    if (str.startsWith(" "))
+      str.remove(0, 1);
+    if (str.startsWith("\""))
+      str.remove(0, 1);
+    if (str.endsWith(" "))
+      str.remove(str.length() - 1, 1);
+    if (str.endsWith("\""))
+      str.remove(str.length() - 1, 1);
+  }
+  return str;
+}
+
+boolean isNumber(String str)
+{
+  for (byte i = 0; i < str.length(); i++)
+  {
+    if (isDigit(str.charAt(i))) return true;
+  }
+  return false;
+}
+
+SMSmessage parseSMSmessage(String response, int index = -1)
+{
+  int pos = 0;
+  int i = 0;
+  String* list = new String[numOfParams];
+  String str = "";
+  for (int j = 0; j < response.length(); j++)
+  {
+    if (response[j] == ',')
+    {
+      if (j + 1 >= response.length())
+      {
+        list[i] = Trim(str);
+        i++;
+      }
+      else
+      {
+        if (response[j + 1] == '\"' || response[j + 1] == ' ')
+        {
+          str = Trim(str);
+          if (i == 0 && !isNumber(str))
+          {
+            list[0] = String(index);
+            i++;
+          }
+          list[i] = str;
+          str = "";
+          i++;
+        }
+        else
+        {
+          str += response[j];
+        }
+      }
+    }
+    else
+    {
+      str += response[j];
+    }
+  }
+  list[i] = Trim(str);
+  SMSmessage msg;
+  msg.Index = list[0].toInt();
+  msg.IsReaded = (list[1].indexOf("UNREAD") == -1);
+  msg.PhoneNumber = list[2];
+  //msg.Date = list[4];
+  msg.SetTime(list[4], true);
+  return msg;
+}
+
+  String GSMSignalLevel(int level) {
+    switch (level) {
+    case 0:
+      return "-115 dBm or less";
+    case 1:
+      return "-111 dBm";
+    case 31:
+      return "-52 dBm or greater";
+    case 99:
+      return "not known or not detectable";
+    default:
+      if (level > 1 && level < 31)
+        return "-110... -54 dBm";
+    }
+    return "Unknown";
+  }
+
+  String GSMRegistrationStatus(RegStatus state) {
+    switch (state) {
+    case REG_UNREGISTERED:
+      return "Not registered, MT is not currently searching a new operator to register to";
+    case REG_SEARCHING:
+      return "Not registered, but MT is currently searching a new operator to register to";
+    case REG_DENIED:
+      return "Registration denied";
+    case REG_OK_HOME:
+      return "Registered, home network";
+    case REG_OK_ROAMING:
+      return "Registered, roaming";
+    case REG_UNKNOWN:
+      return "Unknown";
+    }
+    return "Unknown";
+  }
+
+  String SIMStatus(SimStatus state) {
+    switch (state) {
+    case SIM_ERROR:
+      return "SIM card ERROR";
+    case SIM_READY:
+      return "SIM card is READY";
+    case SIM_LOCKED:
+      return "SIM card is LOCKED. PIN/PUK needed.";
+    }
+    return "Unknown STATUS";
+  }
+
+  void smsInit() {
+    //modemGSM.sendAT(GF("+CPMS=ME,ME,ME"));
+    sendAT(GF("+CPMS=SM,SM,SM"));
+    waitResponse();
+
+    sendAT(GF("+CMGF=1"));
+    waitResponse();
+
+    sendAT(GF("+CNMI=1,0"));
+    waitResponse();
+
+    sendAT(GF("+CSCS=\"GSM\""));
+    waitResponse();
+
+    IsSMSInited = true;
+  }
+
+  typedef std::vector<byte> Outputs;
+  Outputs processIOOHcommand(String message)
+  {
+    Outputs outputs;
+    if (message.startsWith("IOOH"))
+    {
+      int startPos = 0;
+
+      bool detectOutput = false;
+      String output = "";
+      for (int i = 4; i <= message.length(); i++)
+      {
+        if (message[i] == '<')
+        {
+           startPos = i;
+        }
+        else
+        {
+          if (message[i] == 'n' || message[i] == '>')
+          { 
+            if (output.length() > 0)
+            {
+              outputs.push_back(output.toInt());
+              output = "";
+            }
+          }
+          else
+          {
+            if (isDigit(message[i]))
+            {
+              output += message[i];
+            }
+          }
+        }
+      }
+    }
+    return outputs;
+  }
+
+  bool processSMScommand(SMSmessage message)
+  {
+    
+  }
+  
+  bool processSMScommands()
+  {
+      SMSmessages messages = readSMSs();
+      for (const SMSmessage message : messages)
+      {
+        Serial.println("Index: " + String(message.Index));
+        Serial.println("Is readed: " + String(message.IsReaded));
+        Serial.println("Number: " + message.PhoneNumber);
+        Serial.println("Date: " + message.Date);
+        Serial.println("Message: " + message.Message);
+      }
+  }
+  
+  //bool deleteAllSmsMessages(DeleteSmsMode method);
+  bool deleteAllSmsMessages(DeleteSMSMode method) {
+    // Select SMS Message Format: PDU mode. Spares us space now
+    sendAT(GF("+CMGF=0"));
+
+    if (waitResponse() != 1) {
+      return false;
+    }
+
+    sendAT(GF("+CMGDA="), static_cast <
+      const uint8_t > (method));
+    const bool ok = waitResponse(25000L) == 1;
+
+    sendAT(GF("+CMGF=1"));
+    waitResponse();
+
+    return ok;
+  }
+
+  bool deleteSmsMessage(const uint8_t index) {
+    sendAT(GF("+CMGD="), index, GF(","), 0); // Delete SMS Message from <mem1> location
+    return waitResponse(5000L) == 1;
+  }
+
+  SMSmessage readSMS(uint8_t i) 
+  {
+    if (!IsSMSInited)
+      smsInit();
+    //char message[300];
+
+    /*modemGSM.sendAT(GF("+CMGF=1"));
+    modemGSM.waitResponse();
+    modemGSM.sendAT(GF("+CNMI=1,2,0,0,0"));
+    modemGSM.waitResponse();*/
+
+    SMSmessage sms;// = (const struct SMSmessage) { "", "", "" };
+    sms.PhoneNumber = "";
+    sms.Date = "";
+    sms.Message = "";
+
+    sendAT(GF("+CMGR="), i);
+    if (waitResponse(10000L, GF(GSM_NL "+CMGR:"))) 
+    {
+
+      String header = stream.readStringUntil('\n');
+      String body = stream.readStringUntil('\n');
+
+      Serial.println("Header: " + header);
+      Serial.println("Body: " + body);
+
+      //body.toCharArray(message, body.length());
+      //Serial.println("Message: " + String(message));
+      sms = parseSMSmessage(header, i);
+      sms.Message = body;
+    }
+    return sms;
+  }
+
+  typedef std::vector<SMSmessage> SMSmessages;
+  SMSmessages readSMSs(ReadSMSMode mode = ReadSMSMode::AllSMS) {
+    /*modemGSM.sendAT(GF("+CPMS=SM,SM,SM"));
+    modemGSM.waitResponse();
+    modemGSM.sendAT(GF("+CMGF=1"));
+    modemGSM.waitResponse();
+    //modemGSM.sendAT(GF("+CNMI=0,0,0,0,0"));
+    modemGSM.sendAT(GF("+CNMI=1,0,0,0,0"));
+    //modemGSM.sendAT(GF("+CNMI=1,2,0,0,0"));
+    //modemGSM.sendAT(GF("+CNMI=2,2,0,0,0"));
+    modemGSM.waitResponse();*/
+
+    String readMode = "ALL";
+    if (!IsSMSInited)
+      smsInit();
+
+    switch (mode) {
+    case ReadSMSMode::ReceivedUnread:
+      readMode = "REC UNREAD";
+      break;
+    case ReadSMSMode::ReceivedRead:
+      readMode = "REC READ";
+      break;
+    case ReadSMSMode::StoredUnsent:
+      readMode = "STO UNSENT";
+      break;
+    case ReadSMSMode::StoredSent:
+      readMode = "STO SENT";
+      break;
+    case ReadSMSMode::AllSMS:
+      readMode = "ALL";
+      break;
+    }
+
+    SMSmessages messages;
+    sendAT(GF("+CMGL=\"" + readMode + "\""));
+    Serial.println("-----------------------------------");
+    while (true) {
+      if (waitResponse(10000L, GF(GSM_NL "+CMGL:"), GFP(GSM_OK), GFP(GSM_ERROR))) {
+        String header = stream.readStringUntil('\n');
+        SMSmessage msg = parseSMSmessage(header);
+        header.trim();
+        if (header.length() == 0)
+          break;
+        //Serial.println("Header: " + header);
+        String message = stream.readStringUntil('\n');
+        //Serial.println("Message: " + message);
+        msg.Message = message;
+        messages.push_back(msg);
+      } else {
+        break;
+      }
+    }
+    Serial.println("-----------------------------------");
+    return messages;
+  }
+};
